5e2c884fded83413ca9465b0b15ebc34390f4d44
amare1990 <amaremek@gmail.com>
2022-12-22 22:29:18 +0300

    Modify MyEnumerable.rb file

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -4,3 +4,16 @@ module MyEnumerable
       each { |n| bool = false unless yield n }
       bool
     end
+
+    def any?
+      bool = false
+      each { |n| bool = true if yield n }
+      bool
+    end
+
+    def filter
+      arr = []
+      each { |n| arr.push(n) if yield n }
+      arr
+    end
+  end
