20efee0812e326a276ced0e2315a1f82d0f9e55c
luccamicale <luccamicale2015@gmail.com>
2022-12-22 16:22:50 -0300

    initial commit add file Myclasslist

---
+++ MyClassList.rb
@@ -0,0 +1,27 @@
+require_relative 'MyEnumerable'
+
+class MyList
+  include MyEnumerable
+
+  def initialize(*list)
+    @list = list
+  end
+
+  def each
+    count = 0
+    while count < @list.length
+      yield(@list[count])
+      count += 1
+    end
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+
+list.all? { |e| e < 5 }
+list.all? { |e| e > 5 }
+
+list.any? { |e| e == 2 }
+list.any? { |e| e == 5 }
+
+list.filter { |e| e.even? }
